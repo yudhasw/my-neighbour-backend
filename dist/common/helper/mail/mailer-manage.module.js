a7a11fe6d3a5321e53e8a582a9354730e97b822b
Ibnu Syawal Aliefian <superglidingogre0571@gmail.com>
2025-08-21 22:57:02 +0700

    [ADD]; Initating Mailing Module in app

--- dist/common/helper/mail/mailer-manage.module.js
+++ dist/common/helper/mail/mailer-manage.module.js
@@ -10,13 +10,30 @@ exports.MailerManageModule = void 0;
 const common_1 = require("@nestjs/common");
 const mailer_manage_service_1 = require("./mailer-manage.service");
 const mailer_manage_controller_1 = require("./mailer-manage.controller");
+const database_module_1 = require("../../database/database.module");
+const employee_manage_module_1 = require("../../../modules/user-manage-module/employee-module/employee-manage.module");
+const resident_manage_module_1 = require("../../../modules/user-manage-module/resident-module/resident-manage.module");
+const employee_manage_service_1 = require("../../../modules/user-manage-module/employee-module/employee-manage.service");
+const resident_manage_service_1 = require("../../../modules/user-manage-module/resident-module/resident-manage.service");
+const mailer_1 = require("@nestjs-modules/mailer");
 let MailerManageModule = class MailerManageModule {
 };
 exports.MailerManageModule = MailerManageModule;
 exports.MailerManageModule = MailerManageModule = __decorate([
     (0, common_1.Module)({
+        imports: [
+            database_module_1.DatabaseModule,
+            employee_manage_module_1.EmployeeManageModule,
+            resident_manage_module_1.ResidentManageModule,
+            mailer_1.MailerModule.forRootAsync({}),
+        ],
         controllers: [mailer_manage_controller_1.MailerManageController],
-        providers: [mailer_manage_service_1.MailerManageService],
+        providers: [
+            mailer_manage_service_1.MailerManageService,
+            employee_manage_service_1.EmployeeManageService,
+            resident_manage_service_1.ResidentManageService,
+        ],
+        exports: [mailer_manage_service_1.MailerManageService],
     })
 ], MailerManageModule);
 //# sourceMappingURL=mailer-manage.module.js.map
--- src/common/helper/mail/mailer-manage.module.ts
+++ src/common/helper/mail/mailer-manage.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { MailerManageService } from './mailer-manage.service';
 import { MailerManageController } from './mailer-manage.controller';
+import { DatabaseModule } from 'src/common/database/database.module';
+import { EmployeeManageModule } from 'src/modules/user-manage-module/employee-module/employee-manage.module';
+import { ResidentManageModule } from 'src/modules/user-manage-module/resident-module/resident-manage.module';
+import { EmployeeManageService } from 'src/modules/user-manage-module/employee-module/employee-manage.service';
+import { ResidentManageService } from 'src/modules/user-manage-module/resident-module/resident-manage.service';
+import { MailerModule } from '@nestjs-modules/mailer';
+// import { GeneralHelper } from '../generalHelper';
 
 @Module({
+  imports: [
+    DatabaseModule,
+    EmployeeManageModule,
+    ResidentManageModule,
+    MailerModule.forRootAsync({}),
+  ],
   controllers: [MailerManageController],
-  providers: [MailerManageService],
+  providers: [
+    MailerManageService,
+    EmployeeManageService,
+    ResidentManageService,
+  ],
+  exports: [MailerManageService],
 })
 export class MailerManageModule {}
